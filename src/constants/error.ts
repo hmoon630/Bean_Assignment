7be53a662a8c75c50caa3979f78509042f95b19f
hmoon630 <s19017@gsm.hs.kr>
2020-01-06 23:12:31 +0900

    [ADD] Add Error Code & Messages

---
+++ src/constants/error.ts
@@ -0,0 +1,11 @@
+export const INVALID_REQUEST_BODY_FORMAT = {
+    statusCode: 422,
+    code: '000',
+    message: '요청한 데이터의 형식이 올바르지 않습니다.'
+}
+
+export const EXISTING_ID = {
+    statusCode : 422,
+    code : '001',
+    message : '이미 존재하는 아이디입니다.'
+}
