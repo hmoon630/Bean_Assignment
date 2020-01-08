e8646e312cecaaf1ad1cb53af8b740c6d09f5eab
hmoon630 <s19017@gsm.hs.kr>
2020-01-08 23:28:37 +0900

    [ADD] Add Token Authentication Errors

--- src/constants/error.ts
+++ src/constants/error.ts
@@ -15,3 +15,15 @@ export const INVALID_ACCOUNT = {
     code: '002',
     message: '아이디나 패스워드가 올바르지 않습니다.'
 }
+
+export const AUTH_REQUIRED = {
+    statusCode : 401,
+    code: '003',
+    message: '이용하기 위해서는 권한이 필요합니다.'
+}
+
+export const INVALID_TOKEN = {
+    statusCode : 422,
+    code : '004',
+    message: '올바르지 않은 인증 토큰입니다.'
+}
